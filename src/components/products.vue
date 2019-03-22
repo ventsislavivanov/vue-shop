f560e388e569c23df1ea13765648655ff0a207a1
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2019-03-22 17:09:02 +0200

    add filters

--- src/components/products.vue
+++ src/components/products.vue
@@ -8,24 +8,108 @@
           <h2>Filters</h2>
 
           <h3>Categories</h3>
-          <ul>
-            <li v-for="category in categories">{{category}}</li>
-          </ul>
+          <div class="checkboxes-categories">
+            <div class="form-group">
+              <input type="checkbox" value="pants" v-model="categories" id="checkbox-pants"/>
+              <label for="checkbox-pants">Pants</label>
+            </div>
 
-          <h3>Models</h3>
-          <ul>
-            <li v-for="brand in brands">{{brand}}</li>
-          </ul>
+            <div class="form-group">
+              <input type="checkbox" value="t-shirt" v-model="categories" id="checkbox-t-shirts"/>
+              <label for="checkbox-t-shirts">T-shirts</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="jakets" v-model="categories" id="checkbox-jakets"/>
+              <label for="checkbox-jakets">Jakets</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="gloves" v-model="categories" id="checkbox-gloves"/>
+              <label for="checkbox-gloves">Gloves</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="boots" v-model="categories" id="checkbox-boots"/>
+              <label for="checkbox-boots">Boots</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="blouses" v-model="categories" id="checkbox-blouses"/>
+              <label for="checkbox-blouses">Blouse</label>
+            </div>
+          </div>
+
+          <h3>Brands</h3>
+          <div class="checkboxes-brands">
+            <div class="form-group">
+              <input type="checkbox" value="adidas" v-model="brands" id="checkbox-adidas"/>
+              <label for="checkbox-adidas">Adidas</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="nike" v-model="brands" id="checkbox-nike"/>
+              <label for="checkbox-nike">Nike</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="reebok" v-model="brands" id="checkbox-reebok"/>
+              <label for="checkbox-reebok">Reebok</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="puma" v-model="brands" id="checkbox-puma"/>
+              <label for="checkbox-puma">Puma</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="la-sportiva" v-model="brands" id="checkbox-la-sportiva"/>
+              <label for="checkbox-la-sportiva">La Sportiva</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="scarpa" v-model="brands" id="checkbox-scarpa"/>
+              <label for="checkbox-scarpa">Scarpa</label>
+            </div>
+          </div>
 
           <h3>Sizes:</h3>
-          <ul>
-            <li v-for="size in sizes">{{size}}</li>
-          </ul>
+          <div class="checkboxes-brands">
+            <div class="form-group">
+              <input type="checkbox" value="s" v-model="sizes" id="checkbox-s"/>
+              <label for="checkbox-s">S</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="m" v-model="sizes" id="checkbox-m"/>
+              <label for="checkbox-m">M</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="l" v-model="sizes" id="checkbox-l"/>
+              <label for="checkbox-l">L</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="xl" v-model="sizes" id="checkbox-xl"/>
+              <label for="checkbox-xl">XL</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="xxl" v-model="sizes" id="checkbox-xxl"/>
+              <label for="checkbox-xxl">XXL</label>
+            </div>
+
+            <div class="form-group">
+              <input type="checkbox" value="xxxl" v-model="sizes" id="checkbox-xxxl"/>
+              <label for="checkbox-xxxl">XXXL</label>
+            </div>
+          </div>
         </div><!--/.filters -->
 
         <div class="products">
           <div class="products-inner">
-            <div class="product" v-for="product in products">
+            <div class="product" v-for="product in filtredProducts">
               <div class="product-img">
                 <img v-bind:src="getImgUrl(product.image)" />
               </div>
@@ -33,7 +117,7 @@
               <div class="product-content">
                 <h3>{{product.title}}</h3>
                 <p>{{product.brand}}</p>
-                <p>Size: <em>{{product.size}}</em></p>
+                <p>Size: <em>{{product.size | upper}}</em></p>
                 <p>Price: <strong>{{product.price}}</strong></p>
               </div>
             </div>
@@ -51,51 +135,72 @@
   export default {
     data() {
       return {
-        products:{
-          1: {
+        products:[
+          {
             id: 1,
             title: 'T-shirt La Sportiva',
-            brand: 'La Sportiva',
-            size: 'M',
+            brand: 'la-sportiva',
+            size: 's',
             category: 't-shirt',
             price: '50',
             image: 'la-sportiva-t-shirt.jpg',
           },
-          2: {
+          {
             id: 2,
             title: 'T-shirt Scarpa',
-            brand: 'Scarpa',
-            size: 'M',
+            brand: 'scarpa',
+            size: 'm',
             category: 't-shirt',
             price: '45',
             image: 'scarpa-t-shirt.jpg',
           },
-          3: {
+          {
             id: 2,
             title: 'T-shirt Adidas',
-            brand: 'Adidas',
-            size: 'M',
+            brand: 'adidas',
+            size: 'l',
             category: 't-shirt',
             price: '50',
             image: 'addidas-t-shirt.jpg',
           },
-          4: {
+          {
             id: 4,
             title: 'T-shirt Puma',
-            brand: 'Puma',
-            size: 'M',
+            brand: 'puma',
+            size: 'xl',
             category: 't-shirt',
             price: '43',
             image: 'puma-t-shirt.jpg',
           },
-        },
-        categories: ['pants', 't-shirts', 'jakets', 'gloves', 'boots', 'blouses'],
-        brands: ['adidas', 'nike', 'reebok', 'puma', 'la sportiva', 'tribord', 'scarpa'],
-        sizes: ['S', 'M', 'L', 'XL', 'XXL']
+        ],
+        categories: [],
+        brands: [],
+        sizes: []
       }
     },
     methods:{
 
+    },
+    computed:{
+      filtredProducts:function () {
+        return this.products.filter((product) => {
+          if (this.brands.length !== null) {
+            return product.brand.match(this.brands);
+          }
+          if (this.sizes.length !== null) {
+            console.log(this.sizes);
+            return product.size.match(this.sizes);
+          }
+          if (this.categories.length !== null) {
+            return product.category.match(this.categories);
+          }
+        });
+      }
+    },
+    filters:{
+      'upper':function (value) {
+        return value.toUpperCase();
+      }
     },
     mixins: [getUrlImg]
   }
@@ -103,9 +208,10 @@
 
 <style scoped>
   .container {display: flex; justify-content: space-between; flex-wrap: wrap;}
-  .filters { flex: 0 0 20%; background: red;}
+  .filters { flex: 0 0 20%; background: lightgray; padding: 10px;}
   .products { flex: 0 0 80%;}
   .products-inner {display: flex; justify-content: space-between; flex-wrap: wrap;}
+  h3 {padding: 10px;}
   .product-img {flex: 1 0 50%;}
   .product-content {flex: 1 0 50%; text-align: center;}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routes from './routes'
+import Paginate from'vuejs-paginate'
 
 //Packages
 Vue.use(VueRouter);
+
+//Register Components
+Vue.component('paginate', Paginate);
 
 const router = new VueRouter({
   routes: Routes,
