fbdddbf2b90275ae77fa8ba8ada801266b71c53d
Ventsislav Ivanov <ventsislav.ivanov89@gmail.com>
2019-04-01 14:29:11 +0300

    remove paginate plugin

--- src/main.js
+++ src/main.js
@@ -2,13 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import Routes from './routes'
-import Paginate from 'vuejs-paginate'
+
 
 //Packages
 Vue.use(VueRouter);
 
-//Register Components
-Vue.component('paginate', Paginate);
 
 const router = new VueRouter({
   routes: Routes,
@@ -18,10 +16,5 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   render: h => h(App),
-  router: router,
-  methods: {
-    clickCallback: function(pageNum) {
-      console.log(pageNum)
-    }
-  }
+  router: router
 });
